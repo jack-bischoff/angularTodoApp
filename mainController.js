e0cb3468c1e082389746ad5cd5bd7aee107924b6
Mr-Foots <jackbischoff12@gmail.com>
2015-08-09 10:28:33 -0500

    First commit. Includes working app

---
+++ mainController.js
@@ -0,0 +1,29 @@
+angular.module('app', []).controller('mainController', function(){
+
+	function Todo(text){
+		this.text = text
+	}
+
+	var vm = this;
+	vm.todos = []
+
+	vm.createTodo = function(){
+		vm.emptyTodo = false
+		vm.repeatTodo = false
+
+		if (!vm.newTodo){
+			vm.emptyTodo = true
+			return
+		}
+		for (var i=0; i < vm.todos.length; i++){
+			if (vm.todos[i].text == vm.newTodo){
+				vm.repeatTodo = true;
+				vm.newTodo = '';
+				return
+			}
+		}
+		vm.todos.push(new Todo(vm.newTodo));
+		vm.newTodo = ''
+	}
+})
+
